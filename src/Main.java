fc227badc330e71fbca241a1a27ed8e59f73bef0
rostik <rostikcheremys@gmail.com>
2025-04-11 17:38:50 +0300

    completed

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,66 @@
 import java.util.Random;
 
 public class Main {
+    private static final int NUMBER_OF_THREADS = 4;
+    private final Random random = new Random();
+
     public static void main(String[] args) {
-        Random random = new Random();
+        new Main().start();
+    }
 
-        for (int i = 1; i <= 4; i++) {
-            int time = 15000 + random.nextInt(15000);
+    private void start() {
+        ThreadInfo[] threads = new ThreadInfo[NUMBER_OF_THREADS];
 
-            ThreadInfo info = new ThreadInfo(i, time);
-            Thread thread = new MyThread(info);
+        for (int i = 0; i < NUMBER_OF_THREADS; i++) {
+            int threadId = i + 1;
+            int timeInMillis = 10000 + random.nextInt(20000);
+
+            threads[i] = new ThreadInfo(threadId, timeInMillis);
+            System.out.printf("Потiк №%d почав виконання на %.1f сек.\n", threadId, timeInMillis / 1000.0);
+        }
+
+        for (ThreadInfo info : threads) {
+            Thread thread = new Thread(() -> counter(info));
+            info.setThread(thread);
+            info.setStartTime(System.currentTimeMillis());
             thread.start();
+        }
+
+        Thread managerThread = new Thread(() -> manageThreads(threads));
+        managerThread.start();
+    }
+
+    private void counter(ThreadInfo info) {
+        long sum = 0;
+        long count = 0;
+
+        info.setStartTime(System.currentTimeMillis());
+
+        while (info.shouldStop()) {
+            sum += count;
+            count++;
+        }
+
+        System.out.printf("\n[Потiк #%d] зупинено через %.1f сек.\n", info.getId(), info.getTimeInSeconds() / 1000.0);
+        System.out.println("Кiлькiсть елементiв: " + count);
+        System.out.println("Сума: " + sum);
+    }
+
+    private void manageThreads(ThreadInfo[] threadInfo) {
+        boolean isFinished = false;
+
+        while (!isFinished) {
+            isFinished = true;
+
+            for (ThreadInfo info : threadInfo) {
+                if (info.shouldStop() && System.currentTimeMillis() - info.getStartTime() >= info.getTimeInSeconds()) {
+                    info.setShouldStop(true);
+                }
+
+                if (info.shouldStop()) {
+                    isFinished = false;
+                }
+            }
         }
     }
 }
--- src/ThreadInfo.java
+++ src/ThreadInfo.java
@@ -1,1 +1,44 @@
-public record ThreadInfo(int id, int timeToRun) { }
+public class ThreadInfo {
+    private final int id;
+    private final int timeInSeconds;
+    private Thread thread;
+    private long startTime;
+    private volatile boolean shouldStop;
+
+    public ThreadInfo(int id, int timeInMillis) {
+        this.id = id;
+        this.timeInSeconds = timeInMillis;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getTimeInSeconds() {
+        return timeInSeconds;
+    }
+
+    public void setThread(Thread thread) {
+        this.thread = thread;
+    }
+
+    public Thread getThread() {
+        return thread;
+    }
+
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public boolean shouldStop() {
+        return !shouldStop;
+    }
+
+    public void setShouldStop(boolean shouldStop) {
+        this.shouldStop = shouldStop;
+    }
+}
